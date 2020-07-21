f2a0309e84f1b8a6f8f9b960e45c81caeae3eff9
rubandass jhondass <jhonr1@student.op.ac.nz>
2020-07-21 12:20:59 +1200

    Completed section 2

--- Week1Lab1-ex-Select.sql
+++ Week1Lab1-ex-Select.sql
@@ -34,18 +34,24 @@ Databases 3 : Using Transact-SQL : Exercises
 --1.5	Develop a statement that returns every unique person given name only once
 	select distinct GivenName from Person
 
-Exercises for section 2
+--Exercises for section 2
 
-2.1	Develop a statement that returns semester ID and start date
+/* 2.1	Develop a statement that returns semester ID and start date
 	of all semesters in reverse chronological order of their start date
+*/
+	select SemesterID, StartDate from Semester order by StartDate desc 
 
-2.2	Develop a statement that lists each different family name (on the Person table)
+/* 2.2	Develop a statement that lists each different family name (on the Person table)
 	sorted according to the length of the family name with the longest family names first.
 	If different family names have the same length, they must be listed alphabetically.
+*/
+	select distinct len(FamilyName) as [Name Length], FamilyName from Person order by len(FamilyName) desc, FamilyName asc
 
-2.3	Develop a statement that returns the semester ID of the three shortest semesters
+/* 2.3	Develop a statement that returns the semester ID of the three shortest semesters
 	based on the number of days in the semester. Include all semesters that tie for third shortest.
-	
+*/
+	select top 3 with ties SemesterID from Semester order by datediff(day, StartDate, EndDate)
+
 	*******************************************
 	Exercises for section 3
 
