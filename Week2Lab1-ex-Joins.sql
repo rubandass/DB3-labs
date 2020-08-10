5b4227346624faf2911f8167816dee0f74fe07b2
rubandass jhondass <jhonr1@student.op.ac.nz>
2020-08-10 12:43:24 +1200

    week2-lab1 updated

--- Week2Lab1-ex-Joins.sql
+++ Week2Lab1-ex-Joins.sql
@@ -4,16 +4,11 @@ Exercises for section 4
 delete from PaperInstance where PaperID = 'IN610' -- I populated this table using a cross join so all of the data will match otherwise.
 insert into Enrolment values ('IN511', '2019S2', 102);
 
-select * from Paper
-select * from PaperInstance
-select * from Semester
-select * from Person
-select * from Enrolment
 
 4.1	List the starting date and ending date of each occasion 
 	IN511 Programming 2 has run.
 
-	select s.StartDate, s.EndDate from Semester as s
+	select i.PaperID, s.StartDate, s.EndDate from Semester as s
 	join PaperInstance i on s.SemesterID = i.SemesterID
 	where i.PaperID = 'IN511'
 
@@ -23,15 +18,14 @@ select * from Enrolment
 
 	select distinct p.FullName from Person as p
 	join Enrolment e on p.PersonID = e.PersonID
-	where e.PaperID != 'IN511'
+	where e.PaperID = 'IN511'
 
 4.3	List all the full names of all the people who have never enrolled in a paper
 	(according to the data on the database).
 
 	select distinct p.FullName, p.PersonID from Person p
 	left join Enrolment e on p.PersonID = e.PersonID
-	left join Paper pa on e.PaperID = pa.PaperID
-	where e.PaperID IS NULL
+	where e.PersonID IS NULL
 
 4.4	List all the papers that have never been run (according to the data).There are currently none so insert one in order to test the query.
 	
@@ -52,19 +46,15 @@ select * from Enrolment
 	Display the full name of each person and the year in which they enrolled. 
 	Ensure the people are listed alphabetically according to their family name then given name.
 
-	select * from Person p
+	select p.PersonID, p.FullName, year(s.StartDate) as [Enrolled Year] from Person p
 	join Enrolment e on p.PersonID = e.PersonID
 	join Semester s on e.SemesterID = s.SemesterID
 	where e.PaperID = 'IN511' and s.StartDate between '12-apr-2018' and '13-aug-2019'
+	order by p.FamilyName, p.GivenName
 
 
 Exercises for section 5
 
-	select * from Paper
-select * from PaperInstance
-select * from Semester
-select * from Person
-select * from Enrolment
 
 5.1	List all the papers that have a paper instance. 
 	Display the PaperID and number of instances of the paper.
@@ -75,7 +65,7 @@ select * from Enrolment
 5.2	How many people, in total over all semesters, have enrolled in each of the papers
 	that have been delivered? Display the paper ID, paper name and enrolment count.
 
-	select e.PaperID, p.PaperName, count(*) as [Enrolment Count] from Enrolment e
+	select e.PaperID, p.PaperName, count(e.PaperID) as [Enrolment Count] from Enrolment e
 	join Paper p on e.PaperID = p.PaperID
 	join Semester s on e.SemesterID = s.SemesterID
 	where s.EndDate < getdate()
@@ -92,19 +82,18 @@ select * from Enrolment
 	Display the paper instance's start date, en
 	d date, paper name and enrolment count.
 
-	select top 1 e.PaperID, s.StartDate, s.EndDate, p.PaperName, count(*) as [Enrolment Count] from Enrolment e
+	select s.StartDate, s.EndDate, p.PaperName, count(e.PaperID) as [Enrolment Count] from Enrolment e
 	join Paper p on e.PaperID = p.PaperID
 	join Semester s on e.SemesterID = s.SemesterID
 	group by e.PaperID, p.PaperName, s.StartDate, s.EndDate
-	order by count(*) desc
+	order by [Enrolment Count] desc
 
 5.5	List all the people who have 3, 4 or 5 enrolments.
 
-	select ta.PersonID, ta.FullName, ta.[Enrolment Count] from
-	(select e.PersonID, p.FullName, count(*) as [Enrolment Count] from Enrolment e
+	select e.PersonID, p.FullName, count(e.PersonID) as [Enrolment Count] from Enrolment e
 	join Person p on e.PersonID = p.PersonID
-	group by e.PersonID, p.FullName) as ta
-	where [Enrolment Count] between 3 and 5
+	group by e.PersonID, p.FullName
+	having count(e.PersonID) BETWEEN 3 AND 5
 	order by [Enrolment Count] desc
 
 */
