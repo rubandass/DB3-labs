9d46fc45a95298015606f72629b06b03988da9d6
rubandass jhondass <jhonr1@student.op.ac.nz>
2020-09-14 14:24:46 +1200

    Completed Week7Lab2

--- Week7Lab2-UDF.sql
+++ Week7Lab2-UDF.sql
@@ -2,9 +2,6 @@
 Using Transact-SQL : Exercises
 ------------------------------------------------------------
 
-		
-
-
 Exercises for section 15 : UDF
 
 Create a UDF on your database that references the AdventureWorks2016 database. Note you will have to reference AdventureWorks2016 using the FQDN as you only have select over it
@@ -35,21 +32,19 @@ Sales.Customer
 	SELECT * FROM getSales(934)
 
 
-	*************************
-
-	SELECT TOP 3 * FROM AdventureWorks2016.Sales.Customer
-	SELECT TOP 3 * FROM AdventureWorks2016.Production.Product
-	SELECT TOP 3 * FROM AdventureWorks2016.Sales.SalesOrderDetail
-	SELECT TOP 3 * FROM AdventureWorks2016.Sales.SalesOrderHeader
-		
+	***************	For my reference	*************************
+	Manually Checking the query output
 
 		SELECT * FROM AdventureWorks2016.Sales.Customer
 		WHERE storeID = 934
+		Outputs:
 		customerID = 1,29773
 
 		SELECT * FROM AdventureWorks2016.Sales.SalesOrderHeader
 		where CustomerID IN (1,29773)
+		Outputs:
 		SalesOrderID = 43860, 44501, 45283, 46042
 
 		SELECT SUM(OrderQty) FROM AdventureWorks2016.Sales.SalesOrderDetail
 		where SalesOrderID IN (43860, 44501, 45283, 46042)
+	*******************************************************
