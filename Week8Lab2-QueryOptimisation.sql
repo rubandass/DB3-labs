27ea5e8505f609cf95846d6e89f78380abcb9c6b
rubandass <jrubandass@gmail.com>
2020-09-22 08:15:18 +1200

    Week8Lab1,2 done

---
+++ Week8Lab2-QueryOptimisation.sql
@@ -0,0 +1,36 @@
+﻿
+
+USE AdventureWorks2016
+
+/* Approach 1 */
+SELECT MIN(OrderQty) ​
+
+FROM Sales.SalesOrderDetail ​
+
+WHERE OrderQty IN​
+
+	(SELECT   TOP 2 OrderQty ​
+
+	FROM Sales.SalesOrderDetail ​
+
+	ORDER BY OrderQty Desc​
+
+	)​
+
+/* Approach 2 */
+
+SELECT MIN(OrderQty) FROM (SELECT   TOP 2 OrderQty ​
+
+	FROM Sales.SalesOrderDetail ​
+
+	ORDER BY OrderQty Desc​) AS Sales
+
+/* Approach 3 */
+
+SELECT OrderQty ​
+FROM Sales.SalesOrderDetail ​
+ORDER BY OrderQty Desc
+OFFSET 1 ROW 
+FETCH NEXT 1 ROW ONLY
+
+
